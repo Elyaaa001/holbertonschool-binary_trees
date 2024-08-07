b0690b32d3856a78f84bbe886b25f73230b38631
Elyaaa001 <gabibovaelina75gmail.com>
2024-08-07 12:17:38 +0400

    1

---
+++ binary_trees.h
@@ -0,0 +1,13 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
