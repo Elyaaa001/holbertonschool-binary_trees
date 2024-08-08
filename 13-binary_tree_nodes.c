5ecfcbd8495319ab1515cb7b5c31edc4cb186f49
zahraiva <zaraiv06@gmail.com>
2024-08-08 00:48:33 +0000

    fix

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,7 +10,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t n = 0;
 
-	if (tree)
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
 	{
 		n = (tree->left || tree->right) ? 1 : 0;
 		n = binary_tree_nodes(tree->left);
