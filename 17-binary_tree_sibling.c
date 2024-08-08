e8dedf1417110796e764a3b01702a2238a4df407
Elyaaa001 <gabibovelina75@gmail.com>
2024-08-08 17:21:00 +0400

    1

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,6 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
- *
  * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
@@ -11,11 +10,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
+	if (node == node->parent->left)
 		return (node->parent->right);
-
-	if (node->parent->right == node)
+	else
 		return (node->parent->left);
-
-	return (NULL);
 }
+
